fa1256273b70c3a897de20fee6e8fd9b069fc28f
Rakesh7890v <147958244+Rakesh7890v@users.noreply.github.com>
2024-02-09 18:38:37 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Routes, Route, Navigate, useNavigate } from 'react-router-dom';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 import './App.css';
 import Header from './Header';
 import Home from './Home';
